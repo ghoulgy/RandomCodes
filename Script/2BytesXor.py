b7d6ba8fbbaae1b901619df356a454ebb2d3f473
ghoulgy <cyjien@hotmail.com>
2018-12-02 20:40:04 +0800

    Xor for Double Hex

---
+++ Script/2BytesXor.py
@@ -0,0 +1,19 @@
+'''
+' Two Bytes Xored Script
+' By CYJ
+'''
+
+bytesStr = "EE A2 DF EA DC A6 DB AD 9A A3 C9 EA C9 FB D7 BA D6 F9 8A A8 DC BF C9 A9 8E 9E 8B FA D4 8A D1 A3 D9 BE D7 EA 80 E3"
+bytesArray = bytesStr.split()
+key = "BACA"
+flag = ""
+for Sbytes in range(1, len(bytesArray), 2):
+  Dbytes = bytesArray[Sbytes-1] + bytesArray[Sbytes]  # Dbytes 2 bytes e.g. EEA2
+  # print(Dbytes)
+  xored = int(key, 16) ^ int(Dbytes, 16)
+  hexStr = hex(xored)[2:]
+  # print(e)
+  f = bytes.fromhex(hexStr).decode('utf-8')
+  flag += f
+
+print(flag)
