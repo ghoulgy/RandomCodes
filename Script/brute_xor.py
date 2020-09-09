242822393aac945044d610ead4c23b3dbdb93b82
cyjien <cyjien@hotmail.com>
2020-09-09 19:37:57 +0800

    Emotet JP msg pwd extractor

--- Script/brute_xor.py
+++ Script/brute_xor.py
@@ -1,4 +1,6 @@
-with open("filename.bin", "rb") as f:
+import sys
+
+with open("C:\\Users\\cyjien\\Desktop\\possible_conf.bin", "rb") as f:
   contents = f.read()
 
 for i in range(256):
@@ -6,12 +8,13 @@ for i in range(256):
   for content in contents:
     xored += chr(ord(content) ^ i)
 
-  if "Keywords" in xored: # Keyword here
+  if "libgcj" in xored: # Keyword here
     print("Key in decimal {}\nKey in hex {}".format(i, hex(i)))
 
-    with open("xored.bin", "wb") as o:
+    with open("C:\\Users\\cyjien\\Desktop\\xored.bin", "wb") as o:
       o.write(xored)
+      o.close()
+    # sys.exit(0)
 
-o.close()
 f.close()
-
+print("End")
---
+++ Script/emo_jp_pwd_msg_extractor.py
@@ -0,0 +1,23 @@
+# Emotet msg email password extractor
+# Date: 9/2020 
+# By: @ghoul
+import binascii
+import codecs
+import sys 
+import re
+
+with open(sys.argv[1], "rb") as f:
+  content = f.read()
+f.close()
+
+hex_content = binascii.hexlify(content).upper()
+
+match_password_regex = re.compile(b"0D000A00D130B930EF30FC30C9301AFF5B(.*?)5D")
+is_pwd = match_password_regex.search(hex_content)
+
+if is_pwd:
+  pwd_ascii = re.sub(b"00", b'', is_pwd.group(1))
+  pwd_str = codecs.decode(pwd_ascii, 'hex').decode()
+  print(f"{pwd_str}")
+else:
+  print("Pwd Not found")
